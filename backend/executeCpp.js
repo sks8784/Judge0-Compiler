c2bf1303f7d446d8a543762e38bff85a466f4340
Shubham Kumar Sinha <shubhamsinhaasus@gmail.com>
2023-07-12 01:40:47 +0530

    new changes

--- backend/executeCpp.js
+++ backend/executeCpp.js
@@ -18,7 +18,7 @@ const executeCpp = async (filepath,input) => {
 
       // `cd ${filepath}\\.. && g++ ${jobId}.cpp -o abc.exe && abc.exe`,
       // `cd ${filepath}\\.. && g++ ${jobId}.cpp -o abc.exe && (echo ${input}) | abc.exe`,
-      `cd codes && g++ ${jobId}.cpp -o abc.exe && (echo ${input}) | abc.exe`,
+      `cd codes && g++ ${jobId}.cpp && (echo ${input}) | a.exe`,
       (error, stdout, stderr,stdin) => {
         error && reject({ error, stderr });
         stderr && reject(stderr);
